bf5d9855a6005501206272e536dffa8a0e703ca5
Andz rivero <andzrivero89@gmail.com>
2022-11-02 16:10:18 -0800

    working

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
-let textContent = document.querySelector('#textContent')
+let textContent = document.querySelector('body')
 
 let saveText = 'Dawn Andrew N. Rivero'
-
+let k = '-name'
 
 let targetTextInput = document.querySelector('#targetTextInput')
 let input = document.querySelector('#input')
 let keyInput = document.querySelector('#key')
-
-
 let l;
 
-textContent.addEventListener('keyup', (e)=>{
-    var key = keyInput.value
-    let curPos =  textContent.selectionStart;
-    // will get the value of the text area
-    let x= textContent.value;
-    // will get the value of the input box  
+let curPos;
 
+
+textContent.addEventListener("click",(e)=>{
+    l = e.target.value.length 
+
+})
+
+textContent.addEventListener('keyup', (e)=>{
+    if(e.target.value === undefined){
+        return false
+    }
+
+    
+    var key = k
+    curPos =  e.target.selectionStart;
+ 
+    // will get the value of the text area
+    let x= e.target.value;
+    // will get the value of the input box  
+   
     let a =  e.target.value.slice(l)
+    
     targetTextInput.value = a
 
-    if(e.keyCode === 32){
+    if(e.code  ===  'Space' || e.code === 'Enter'){
     targetTextInput.value = ''
-    l = e.target.value.length
+    l = e.target.value.length 
 
-    }
-      
+    }   
+
         
     if( targetTextInput.value  == key){
 
-        textContent.value =  x.slice(0 ,curPos - key.length)+input.value  + ' '
+        e.target.value =  x.slice(0 ,curPos - key.length)+saveText
 
     }
 })
